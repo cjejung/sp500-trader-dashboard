af2a6463c899187fe9c6f7a2799b6b49613fef8a
cjejung <cjejung@gmail.com>
2021-10-04 09:44:49 +0200

    slimmed down version

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import './app/App.scss';
+
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+
 import App from './app/App';
-import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
-  <BrowserRouter basename="/">
-    <App />
-  </BrowserRouter>
+    <div className="az-content-wrapper">
+      <App />
+    </div>
 , document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
