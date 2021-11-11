0ec5d8a2d05015629db9e7bb69c71f5581ccca4c
cjejung <cjejung@gmail.com>
2021-11-11 05:06:59 +0000

    Deploying to gh-pages from @ cjejung/sp500-trader-dashboard@5fc039d697cd10d8ae5255363e4d8892fd9bbb16 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sp500-trader-dashboard/precache-manifest.1e70e7248757c1d6ae05b7785778b469.js"
+  "/sp500-trader-dashboard/precache-manifest.db1b62d7ededb39634ddb671bba63354.js"
 );
 
 self.addEventListener('message', (event) => {
